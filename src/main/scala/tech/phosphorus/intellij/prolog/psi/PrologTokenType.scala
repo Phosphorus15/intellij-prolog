0a3ba38276a71fdd28c21d026ea8bca3bb32f67f
Phosphorus15 <steepout@qq.com>
2020-03-15 00:47:34 +0800

    Injected stubs into `PrologTypes`

--- src/main/scala/tech/phosphorus/intellij/prolog/psi/PrologTokenType.scala
+++ src/main/scala/tech/phosphorus/intellij/prolog/psi/PrologTokenType.scala
@@ -2,7 +2,17 @@ package tech.phosphorus.intellij.prolog.psi
 
 import com.intellij.psi.tree.IElementType
 import tech.phosphorus.intellij.prolog.PrologLanguage
+import tech.phosphorus.intellij.prolog.toolchain.PrologStubElementType
 
-class PrologTokenType(debugName : String) extends IElementType(debugName, PrologLanguage.INSTANCE)
+class PrologTokenType(debugName: String) extends IElementType(debugName, PrologLanguage.INSTANCE)
 
-class PrologElementType(debugName : String) extends IElementType(debugName, PrologLanguage.INSTANCE)
+class PrologElementType(debugName: String) extends IElementType(debugName, PrologLanguage.INSTANCE)
+
+object PrologElementType {
+  def createType(debugName: String): IElementType = {
+    debugName match {
+      case "PREDICATE_ID" => PrologStubElementType.PREDICATE_ID
+      case _ => new PrologElementType(debugName)
+    }
+  }
+}
