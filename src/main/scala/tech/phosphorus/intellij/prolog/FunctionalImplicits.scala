f669cc0dfbbfb782fa6afd451805107a7734acd5
Phosphorus15 <steepout@qq.com>
2020-03-17 22:30:00 +0800

    奇怪的范式增加了！（猫猫脸

--- src/main/scala/tech/phosphorus/intellij/prolog/FunctionalImplicits.scala
+++ src/main/scala/tech/phosphorus/intellij/prolog/FunctionalImplicits.scala
@@ -3,8 +3,7 @@ package tech.phosphorus.intellij.prolog
 import com.intellij.execution.ExecutionException
 import com.intellij.execution.configurations.GeneralCommandLine
 import com.intellij.execution.process.{CapturingProcessHandler, ProcessOutput}
-import com.intellij.openapi.progress.ProgressIndicator
-import com.intellij.openapi.progress.Task.Backgroundable
+import tech.phosphorus.intellij.prolog.FunctionalImplicits.OptionT
 
 import scala.reflect.ClassTag
 
@@ -12,6 +11,7 @@ import scala.reflect.ClassTag
  * All the black magics here
  */
 object FunctionalImplicits {
+
   implicit class ExecutedGeneralCommandLine(cmd: GeneralCommandLine) {
     def captureOutput(timeout: Int = 1000): ProcessOutput = {
       try {
@@ -21,6 +21,68 @@ object FunctionalImplicits {
       }
     }
   }
+
+  implicit class Nullable[T <: Null](val t: T) {
+    def asOption(): Option[T] = Option(t)
+    def isDefined: Boolean = t != null
+    def isNull: Boolean = !isDefined
+  }
+
+  type Result[T] = Either[T, Exception]
+
+  implicit class ExceptionMonad[T](val t: Result[T]) extends Monad[T, Result] {
+
+    def this(t : T) {
+      this(Left(t))
+    }
+
+    override def >>=[B](f: T => Result[B])(implicit classTag: ClassTag[B]): Result[B] = if(t.isRight) Right(t.right.get) else f(t.left.get)
+
+    override def <*>[B](f: Result[T => B])(implicit classTag: ClassTag[B]): Result[B] = t match {
+      case Left(a) => f match {
+        case Left(f) => Left(f(a))
+        case Right(r) => Right(r)
+      }
+      case Right(r) => Right(r)
+    }
+
+    override def <@>[B](f: T => B)(implicit classTag: ClassTag[B]): Result[B] = if(t.isRight) Right(t.right.get) else Left(f(t.left.get))
+  }
+
+  implicit class OptionT[T](val t: Option[T]) extends Monad[T, Option]{
+    override def <@>[B](f: T => B)(implicit classTag: ClassTag[B]): Option[B] = t.map(f)
+
+    override def >>=[B](f: T => Option[B])(implicit classTag: ClassTag[B]): Option[B] = if (t.isEmpty) None else f(t.get)
+
+    override def <*>[B](f: Option[T => B])(implicit classTag: ClassTag[B]): Option[B] = if (t.isEmpty) None else f.map(_(t.get))
+  }
+
+  implicit class ArrayT[T](val t: Array[T]) extends Monad[T, Array] {
+    override def >>=[B](f: T => Array[B])(implicit classTag: ClassTag[B]): Array[B] = t.flatMap(v => f(v).t).toArray
+
+    override def <*>[B](f: Array[T => B])(implicit classTag: ClassTag[B]): Array[B] = t.flatMap(v => f.map(_(v))).toArray
+
+    override def <@>[B](f: T => B)(implicit classTag: ClassTag[B]): Array[B] = t.map(f).toArray
+  }
+
+}
+
+object OptionLetIn {
+  implicit class LetIn[T](val v: Option[T]) extends OptionT[T](v){
+    def let[B](f: T => B)(implicit classTag: ClassTag[B]): Option[B] = super.<@>(f)
+  }
+}
+
+trait Functor[A, Impl[T]] {
+  def <@> [B](f: A => B)(implicit classTag: ClassTag[B]): Impl[B]
+}
+
+trait Applicative[A, Impl[T]] extends Functor[A, Impl] {
+  def <*> [B](f: Impl[A => B])(implicit classTag: ClassTag[B]): Impl[B]
+}
+
+trait Monad[A, Impl[T]] extends Applicative[A, Impl] {
+  def >>= [B](f: A => Impl[B])(implicit classTag: ClassTag[B]): Impl[B]
 }
 
 object RunnableImplicits {
--- src/main/scala/tech/phosphorus/intellij/prolog/completion/PrologAtomCompletion.scala
+++ src/main/scala/tech/phosphorus/intellij/prolog/completion/PrologAtomCompletion.scala
@@ -2,11 +2,10 @@ package tech.phosphorus.intellij.prolog.completion
 
 import com.intellij.codeInsight.completion.{CompletionParameters, CompletionProvider, CompletionResultSet}
 import com.intellij.codeInsight.lookup.LookupElementBuilder
-import com.intellij.psi.PsiElement
-import com.intellij.testFramework.exceptionCases.IncorrectOperationExceptionCase
 import com.intellij.util.{IncorrectOperationException, ProcessingContext}
 import tech.phosphorus.intellij.prolog.inspector.SingletonAnalysis
-import tech.phosphorus.intellij.prolog.psi.{PrologIdent, PrologPsiUtil, PrologToplevelExpr, PrologTrailingExpr, PrologTypes}
+import tech.phosphorus.intellij.prolog.psi.{PrologPsiUtil, PrologToplevelExpr, PrologTrailingExpr, PrologTypes}
+import tech.phosphorus.intellij.prolog.OptionLetIn._
 
 class PrologAtomCompletion extends CompletionProvider[CompletionParameters]() {
   override def addCompletions(v: CompletionParameters, processingContext: ProcessingContext, completionResultSet: CompletionResultSet): Unit = {
@@ -14,16 +13,17 @@ class PrologAtomCompletion extends CompletionProvider[CompletionParameters]() {
     val project = v.getEditor.getProject
     if (psi == null || project == null) return
     if (psi.getNode.getElementType == PrologTypes.ATOM_ID) {
-      val top = PrologPsiUtil
+      PrologPsiUtil
         .findParentWith(psi, element =>
           element.isInstanceOf[PrologToplevelExpr]
-          || element.isInstanceOf[PrologTrailingExpr]).getOrElse(throw new IncorrectOperationException("unable top locate parent psi element"))
-      SingletonAnalysis.resolveLocalAtoms(top).filterNot(_.getText == psi.getText).filter(_.getText.contains(psi.getText))
-        .foreach(element =>
-          completionResultSet.addElement(
-            LookupElementBuilder.create(element.getText)
+          || element.isInstanceOf[PrologTrailingExpr]).let { top =>
+        SingletonAnalysis.resolveLocalAtoms(top).filterNot(_.getText == psi.getText).filter(_.getText.contains(psi.getText))
+          .foreach(element =>
+            completionResultSet.addElement(
+              LookupElementBuilder.create(element.getText)
+            )
           )
-        )
+      }.getOrElse(throw new IncorrectOperationException("unable top locate parent psi element"))
     }
   }
 }
